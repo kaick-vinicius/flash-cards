fdbb896d6a40c02f9c3572571f0aeefe405d259d
kaick-vinicius <kaickviniciusferreira@gmail.com>
2025-05-12 15:02:34 +0000

    ok

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# flash-cards
-trabalho
+                  
---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'programação',
+'oque é JavaScript',
+'Linguagem de programação'
+)
+criaCartao(
+    'inglês',
+    'como se escreve dia do pagamento?',
+    'Pay Day'
+)
+criaCartao(
+    'Matemática',
+'Qual gráfico da função quadrática?',
+'Parábola'
+)
